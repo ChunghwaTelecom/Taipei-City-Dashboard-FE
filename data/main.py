87ba290a54afd05702f2f893117f9d38dd581158
BigPigKingJames <ericzx103703012@gmail.com>
2023-11-18 13:06:25 +0800

    [Feat](all) Add procedure for processed_postpartum_care_center

---
+++ data/main.py
@@ -0,0 +1,93 @@
+import requests
+import pandas as pd
+from tqdm import tqdm
+from typing import Dict, List
+from urllib.parse import quote
+
+
+def get_map_box_geo_data_from_address(actural_address: str) -> Dict[str, object]:
+    map_box_geo_data = {}
+    map_box_access_token: str = "pk.eyJ1IjoiZXJpY3p4MTAzNzAzMDEyIiwiYSI6ImNscDNqNTlnNTEydXkyanF5emtzc296MmkifQ.eRRCu7Zxrx3MydV_yVjG8Q"  # noqa
+    encoded_actural_address: str = quote(actural_address)
+
+    target_url: str = f"https://api.mapbox.com/geocoding/v5/mapbox.places/{encoded_actural_address}.json?access_token={map_box_access_token}"  # noqa
+
+    response = requests.get(target_url)
+    if response.status_code == 200:
+        data = response.json()
+
+        if "features" in data and len(data["features"]) > 0:
+            map_box_geo_data["center_longitude"] = data["features"][0]["center"][0]
+            map_box_geo_data["center_latitude"] = data["features"][0]["center"][1]
+            map_box_geo_data["locality_id"] = data["features"][0]["context"][2]["id"]
+            map_box_geo_data["locality_text"] = data["features"][0]["context"][2][
+                "text"
+            ]
+        else:
+            map_box_geo_data["center_longitude"] = data["features"][0]["center"][0]
+            map_box_geo_data["center_latitude"] = data["features"][0]["center"][1]
+            map_box_geo_data["locality_id"] = data["features"][0]["context"][2]["id"]
+            map_box_geo_data["locality_text"] = data["features"][0]["context"][2][
+                "text"
+            ]
+
+        return map_box_geo_data
+    else:
+        return map_box_geo_data
+
+
+def parse_postpartum_care_center(file_path: str) -> pd.DataFrame:
+    input_df: pd.DataFrame = pd.read_csv(file_path)
+
+    map_box_geo_data_list: List[Dict[str, object]] = []
+
+    for _, row in tqdm(input_df.iterrows()):
+        map_box_geo_data = get_map_box_geo_data_from_address(row["機構地址"])
+        map_box_geo_data_list.append(map_box_geo_data)
+
+    center_longitudes = [
+        map_box_geo_data["center_longitude"]
+        for map_box_geo_data in map_box_geo_data_list
+    ]
+    center_latitudes = [
+        map_box_geo_data["center_latitude"]
+        for map_box_geo_data in map_box_geo_data_list
+    ]
+    locality_ids = [
+        map_box_geo_data["locality_id"] for map_box_geo_data in map_box_geo_data_list
+    ]
+    locality_texts = [
+        map_box_geo_data["locality_text"] for map_box_geo_data in map_box_geo_data_list
+    ]
+
+    input_df["center_longitude"] = center_longitudes
+    input_df["center_latitudes"] = center_latitudes
+    input_df["locality_ids"] = locality_ids
+    input_df["locality_texts"] = locality_texts
+
+    return input_df
+
+
+def output_postpartum_care_center(processed_df: pd.DataFrame):
+    processed_df.to_csv(
+        "data/clean_data/processed_postpartum_care_center.csv", index=False
+    )
+    processed_json = processed_df.to_json(orient="records", force_ascii=False)
+    with open(
+        "data/clean_data/processed_postpartum_care_center.json", "w", encoding="utf-8"
+    ) as file:
+        file.write(processed_json)
+
+
+def main():
+    postpartum_care_center_file_path = (
+        "data/raw_data/postpartum_care_center_with_money.csv"
+    )
+    processed_df: pd.DataFrame = parse_postpartum_care_center(
+        postpartum_care_center_file_path
+    )
+    output_postpartum_care_center(processed_df)
+
+
+if __name__ == "__main__":
+    main()
