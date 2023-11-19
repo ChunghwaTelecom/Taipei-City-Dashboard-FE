66957d4acc0919ebc9386e4d36d5e260ba7ffab1
Pei-Tang Huang <tangtheone@gmail.com>
2023-11-19 11:56:12 +0800

    Calcaulate radius from data.

--- src/components/charts/BubbleChart.vue
+++ src/components/charts/BubbleChart.vue
@@ -1,9 +1,7 @@
 <script setup>
-import {  defineProps, onUpdated } from "vue";
-import { useMapStore } from "../../store/mapStore";
+import { defineProps, onUpdated } from "vue";
 
-const props = defineProps(["chart_config", "activeChart", "series", "map_config"]);
-const mapStore = useMapStore();
+const props = defineProps(["chart_config", "activeChart", "series"]);
 
 function isOverlapping(newBubble, bubbles) {
 	for (let bubble of bubbles) {
@@ -18,16 +16,16 @@ function isOverlapping(newBubble, bubbles) {
 	return false;
 }
 
-function generateRandomBubble(maxWidth, maxHeight, category) {
-	let radius = Math.random() * 20 + 5; // Random radius between 5 and 25
+function generateRandomBubble(maxWidth, maxHeight, category, value) {
+	let radius = Math.sqrt(value) * 5; // Calculate radius based on the square root of the value
 	let x = Math.random() * (maxWidth - 2 * radius) + radius;
 	let y = Math.random() * (maxHeight - 2 * radius) + radius;
 	return { x: x, y: y, radius: radius, text: category };
 }
 
-function addBubble(bubbles, maxWidth, maxHeight, maxAttempts, category) {
+function addBubble(bubbles, maxWidth, maxHeight, maxAttempts, category, value) {
 	for (let attempts = 0; attempts < maxAttempts; attempts++) {
-		let newBubble = generateRandomBubble(maxWidth, maxHeight, category);
+		let newBubble = generateRandomBubble(maxWidth, maxHeight, category, value);
 		if (!isOverlapping(newBubble, bubbles)) {
 			bubbles.push(newBubble);
 			return;
@@ -44,8 +42,8 @@ function drawBubble(bubble, svg) {
 	circle.setAttribute("fill", "rgba(0, 0, 255, 0.5)"); // Semi-transparent blue
 	circle.setAttribute("stroke", "black");
 	let text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-	text.setAttribute("x", bubble.x+1);
-	text.setAttribute("y", bubble.y+1);
+	text.setAttribute("x", bubble.x + 1);
+	text.setAttribute("y", bubble.y + 1);
 	text.setAttribute("text-anchor", "middle");
 	text.setAttribute("fill", "white");
 	let textContent = document.createTextNode(bubble.text);
@@ -64,15 +62,19 @@ function createBubbleChart() {
 		let chartWidth = svg.getAttribute("width");
 		let chartHeight = svg.getAttribute("height");
 
+		const maxValue = props.series[0].data.reduce((a, b) => Math.max(a, b));
+
 		for (let i = 0; i < numberOfBubbles; i++) {
-			addBubble(bubbles, chartWidth, chartHeight, maxAttempts, props.chart_config.categories[i]);
+			let category = props.chart_config.categories[i];
+			let value = props.series[0].data[i];
+			let radius = (value / maxValue) * 30;
+			addBubble(bubbles, chartWidth, chartHeight, maxAttempts, category, radius);
 		}
 
 		for (let bubble of bubbles) {
 			drawBubble(bubble, svg);
 		}
 	}
-
 }
 
 onUpdated(() => {
@@ -84,7 +86,7 @@ onUpdated(() => {
 <template>
 	<div v-if="activeChart === 'BubbleChart'">
 		<div>
-			<svg id="bubbleChart" width="300" height="280"></svg>
+			<svg id="bubbleChart" width="400" height="240"></svg>
 		</div>
 	</div>
 </template>
